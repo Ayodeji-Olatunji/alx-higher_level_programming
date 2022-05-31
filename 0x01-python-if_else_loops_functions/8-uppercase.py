e4fc8873ed4b695e1e77e46a5f8be25828edeaa0
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-05-31 15:01:14 -0700

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     temp = list(str)
     for s in range(len(temp)):
         if ord(temp[s]) > 96 and ord(temp[s]) < 123:
-            temp[s] = chr(ord(temp[s)] - 32)
+            temp[s] = chr(ord(temp[s]) - 32)
     print("{}".format("".join(tmp)))
