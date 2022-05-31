aac3a765db2d8f78a0322e5023e600e654c3018d
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-05-31 13:22:52 -0700

    Ascii alphabets in lowercase except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for val in "abcdefghijklmnopqrstuvwxyz":
-    if val == 'q' or val == 'e':
-        continue
-print("{}".format(val), end="")
+    if val != 'q' and val != 'e':
+        print("{}".format(val), end="")
