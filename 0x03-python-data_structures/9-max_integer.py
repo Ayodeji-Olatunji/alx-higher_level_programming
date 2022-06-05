0e507bd2aa4bbf8372874bfd9d3fbc41ed8fd2a6
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-06-05 15:35:35 -0700

    Maximum integer in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list != []:
+    if my_list:
         max_num = my_list[0]
         for i in range(0, len(my_list)):
             if my_list[i] > max_num:
                 max_num = my_list[i]
-                return max_num
+        return max_num
     else:
         return None
