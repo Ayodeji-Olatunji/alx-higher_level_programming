28247cd73ea397c5c4c83ee107f7ed31c454e06c
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-06-08 12:28:49 -0700

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for i in my_list:
-        if i != search:
-            new_list[len(new_list):] = [index]
-        else:
-            new_list[len(new_list):] = [replace]
-    return new_list
+    return [replace if search == i else i for i in my_list]
