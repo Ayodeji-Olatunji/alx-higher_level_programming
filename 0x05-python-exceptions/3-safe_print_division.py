7f71aa66cafea08895b9d1ecc9c9ab7fdc591ee3
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-06-20 14:54:39 -0700

    safe print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
-    except ZeroDvisionError as err:
+    except ZeroDivisionError as err:
         result = None
     finally:
         print('Inside result: {}'.format(result))
