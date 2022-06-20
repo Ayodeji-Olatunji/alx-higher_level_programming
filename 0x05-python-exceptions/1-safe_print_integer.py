c00e425bce3fa21e09871ecac63e82c39760137e
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-06-20 13:37:02 -0700

    Safe print integers

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except Exception NotAnInteger:
+    except Exception as err:
         return False
     else:
         return True
