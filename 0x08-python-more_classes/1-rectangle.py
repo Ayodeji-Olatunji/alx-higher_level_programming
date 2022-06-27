b2176411e5a1358c3977d49be719777124a1f690
Ayodeji-Olatunji <Olatunjitobiloba087@gmail.com>
2022-06-27 16:16:52 -0700

    creates a rectangle class with getter and setter methods

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,7 @@ This module creates a Rectangle class
 
 
 class Rectangle:
-    """
-    Defines a class Rectangle with getter and setter methods
+    """Defines a class Rectangle with getter and setter methods
     """
     def __init__(self, width=0, height=0):
         self.width = width
@@ -20,7 +19,8 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value  ValueError("width must be >= 0")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -31,6 +31,6 @@ class Rectangle:
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
